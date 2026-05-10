5534cac552c6156350b61aa6cc86a8b31bd465f8
SANDER SCHEPERS <sander.schepers@student.hogent.be>
2026-05-11 00:44:03 +0200

    Correcting a typo

--- Target/02_setup_server.sh
+++ Target/02_setup_server.sh
@@ -35,15 +35,15 @@ log "Systeem updaten..."
 apt-get update -qq
 apt-get upgrade -y -qq
  
-# --- STAP 2: Java 11 installeren ---
+# --- STAP 2: Java 17 installeren ---
 log "Java 17 (OpenJDK) installeren..."
 apt-get install -y openjdk-17-jdk curl wget unzip git net-tools
  
-java -version 2>&1 | grep "17" && log "Java 11 OK" || err "Java 11 installatie mislukt"
+java -version 2>&1 | grep "17" && log "Java 11 OK" || err "Java 17 installatie mislukt"
  
 # Java 11 als standaard instellen
 update-alternatives --set java /usr/lib/jvm/java-17-openjdk-amd64/bin/java 2>/dev/null || true
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> /etc/environment
  
 # --- STAP 3: SSH installeren en configureren ---
